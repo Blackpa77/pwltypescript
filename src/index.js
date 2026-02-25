02995d6574505745e5966437a12ed300e03a8cbb
Paundra <h1101241029@student.untan.ac.id>
2026-02-25 04:42:16 +0000

    praktikum 1

---
+++ src/index.js
@@ -0,0 +1,7 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+function hello(nama) {
+    return 'selamat datang sayang $(nama)n di pwl';
+}
+console.log(hello('Blackpa'));
+//# sourceMappingURL=index.js.map
