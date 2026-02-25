459431a538c2bb70450d6a19abf2bae1f8adf1b6
Paundra <h1101241029@student.untan.ac.id>
2026-02-25 04:46:13 +0000

    save 2

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 function hello(nama: string, umur: number): string {
     return `selamat datang sayang ${nama} di pwl, umur saya ${umur}`;
 }
-const mahasiswa: string = "Andy";
+const mahasiswa: string = "Paundra";
 const umur: number = 20;
 console.log(hello(mahasiswa, umur));
