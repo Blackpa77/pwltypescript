00885e70bb0e24ab27ffe019ae9d4a1cc5bd1b08
Paundra <h1101241029@student.untan.ac.id>
2026-02-25 04:47:18 +0000

    gani nama# Please enter the commit message for your changes. Lines starting

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 function hello(nama: string, umur: number): string {
     return `selamat datang sayang ${nama} di pwl, umur saya ${umur}`;
 }
-const mahasiswa: string = "Paundra";
+const mahasiswa: string = " Arjun Maheswara Paundra";
 const umur: number = 20;
 console.log(hello(mahasiswa, umur));
